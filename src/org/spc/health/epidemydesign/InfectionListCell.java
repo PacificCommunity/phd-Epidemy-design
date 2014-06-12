1cabfe7db26695bcaf2ca8d89a158fb2f4540f9d
Fabrice Bouyé <fabriceb@spc.int>
2014-06-13 10:36:40 +1100

    Added a list cell to render infections in combobox and listview instances.

---
+++ src/org/spc/health/epidemydesign/InfectionListCell.java
@@ -0,0 +1,22 @@
+/***********************************************************************
+ *  Copyright - Secretariat of the Pacific Community                   *
+ *  Droit de copie - Secrétariat Général de la Communauté du Pacifique *
+ *  http://www.spc.int/                                                *
+ ***********************************************************************/
+package org.spc.health.epidemydesign;
+
+import javafx.scene.control.ListCell;
+
+/**
+ * Displays infections in list and combo box.
+ * @author Fabrice Bouyé (fabriceb@spc.int)
+ */
+final class InfectionListCell extends ListCell<Infection> {
+
+    @Override
+    protected void updateItem(Infection infection, boolean empty) {
+        super.updateItem(infection, empty);
+        final String text = infection == null ? I18N.getString("DEFAULT_VALUE_LABEL") : infection.name; // NOI18N.
+        setText(text);
+    }    
+}
