d7a41308d1e213eaa365f93b55f9605cd5a32dc5
Fabrice Bouyé <fabriceb@spc.int>
2014-06-17 11:04:14 +1100

    Added control to handle state edition.

---
+++ src/org/spc/health/epidemydesign/control/stateeditor/StateEditorController.java
@@ -0,0 +1,32 @@
+/***********************************************************************
+ *  Copyright - Secretariat of the Pacific Community                   *
+ *  Droit de copie - Secrétariat Général de la Communauté du Pacifique *
+ *  http://www.spc.int/                                                *
+ ***********************************************************************/
+package org.spc.health.epidemydesign.control.stateeditor;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * Controller for the state editor.
+ * @author Fabrice Bouyé (fabriceb@spc.int)
+ */
+public final class StateEditorController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    @FXML
+    private void handleStatesDefaultButton(final ActionEvent actionEvent) {
+
+    }
+}
